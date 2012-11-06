50cad74b139e83e6ba99eb94c745647c82c69701
steven <steven@stpad.(none)>
2012-11-06 21:02:03 +0800

    del auto-generated files

--- elpa/logito-20120225.2155/logito-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; logito-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads nil nil ("logito-pkg.el" "logito.el") (20632 63954
-;;;;;;  993286))
-
-;;;***
-
-(provide 'logito-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; logito-autoloads.el ends here
